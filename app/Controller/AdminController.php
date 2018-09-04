5f590a3d03bfabf9af72f35ce4df4bf10fca8a95
nagharaaj <37173342+nagharaaj@users.noreply.github.com>
2018-09-04 16:19:01 +0530

    Update AdminController.php

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -76,45 +76,10 @@ class AdminController extends AppController {
                 $emailList = array('siddharthk@evolvingsols.com');
 		
 		
-$authurl = "https://login.microsoftonline.com/6e8992ec-76d5-4ea5-8eae-b0c5e558749a/oauth2/token/";
 
-$client_id = "96d6293f-922a-4cb0-bbb1-38e58eb16008@6e8992ec-76d5-4ea5-8eae-b0c5e558749a";
-$client_secret = "FXXI8/bRHbpNKjGSwFMb4kM5sRAJbNKUQ1b90b4nD44=";
-
-// Creating base 64 encoded authkey
-$Auth_Key = $client_id.":".$client_secret;
-$encoded_Auth_Key=base64_encode($Auth_Key);
-
-$headers = array();
-$headers['Authorization'] = "Basic ".$encoded_Auth_Key;
-$headers['Content-Type'] = "application/x-www-form-urlencoded";
-
-$data = array(
-    'grant_type' => 'client_credentials',
-    'scope'      => 'read write',
-    'username'   => 'syssp-p-nbrsffeed@dentsuaegis.com',
-    'password'   => 'Password01'
-);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $authurl);
-curl_setopt($ch, CURLOPT_POST, 1 );
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-$auth = curl_exec( $ch );
-
-if ( curl_errno( $ch ) ){
-    echo 'Error: ' . curl_error( $ch );
-}
-curl_close($ch);
-
-$secret = json_decode($auth);
-$access_key = $secret->access_token;
                 //the target url of NBR system.
-               // $siteUrl = 'team.dentsuaegis.com/sites/nbr/';
-                //$userpwd = 'MEDIA\sysSP-P-NBR:Jfo829/K!';
+                $siteUrl = 'team.dentsuaegis.com/sites/nbr/';
+                $userpwd = 'MEDIA\sysSP-P-NBR:Jfo829/K!';
 
                 // curl object for read requests
                 //$ch = curl_init();
